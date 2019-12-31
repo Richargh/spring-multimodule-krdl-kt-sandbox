d9c55d4350ae35d4861367848172e3d1fd6b731c
Richard Gross <richard.gross@maibornwolff.de>
2019-12-31 10:20:57 +0100

    F let Spring manage the dependencies of non-spring components

--- component/catalogue-api/build.gradle.kts
+++ component/catalogue-api/build.gradle.kts
@@ -11,8 +11,6 @@ dependencies {
     // none
 
     /** Test dependencies **/
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
-    testImplementation("com.ninja-squad:springmockk:1.1.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
-
