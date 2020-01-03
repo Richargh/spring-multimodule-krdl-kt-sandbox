97682906886a53a4f33a9a6eaab5fd7e8a30e9ff
Richard Gross <richard.gross@maibornwolff.de>
2020-01-03 22:04:22 +0100

    b: fixed index.html HttpMediaTypeNotAcceptableException by adding converters { resource() }

--- container/rome/build.gradle.kts
+++ container/rome/build.gradle.kts
@@ -27,8 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    runtimeOnly("com.h2database:h2:1.4.200") // See https://github.com/spring-projects/spring-boot/issues/18593 and https://github.com/h2database/h2database/issues/1841
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
 
     /** Test dependencies **/
@@ -38,6 +37,7 @@ dependencies {
     }
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.assertj:assertj-core")
 }
 
 springBoot {
--- container/rome/src/main/kotlin/de/richargh/sandbox/spring/multibuild/rome/Rome.kt
+++ container/rome/src/main/kotlin/de/richargh/sandbox/spring/multibuild/rome/Rome.kt
@@ -8,7 +8,8 @@ import de.richargh.sandbox.spring.multibuild.factory.web.FactoryHandler
 import de.richargh.sandbox.spring.multibuild.factory.web.factoryRoutes
 import de.richargh.sandbox.spring.multibuild.shared_web.config.sharedWebBeans
 import org.springframework.boot.WebApplicationType
-import org.springframework.boot.runApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.boot.logging.LogLevel
 import org.springframework.context.annotation.PropertySource
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.webmvc.mustache
@@ -21,21 +22,29 @@ val app = application(WebApplicationType.SERVLET) {
     sharedWebBeans()
     catalogueBeans()
     factoryBeans()
+
+    listener<ApplicationReadyEvent> {
+        println("Application Ready")
+    }
+
+    logging {
+        level = LogLevel.INFO
+    }
+
     webMvc {
         port = 8081
         router {
             catalogueRoutes(CatalogueHandler())
             factoryRoutes(FactoryHandler())
         }
-        mustache {
-//            this.
-        }
+        mustache { }
         converters {
             string()
+            resource()
         }
     }
 }
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     app.run(args)
 }
